1fbf2d4a210d7fca1b186930c73f74979bfd59c2
nagolove <dekar.own@gmail.com>
2023-11-27 01:27:42 +0300

    Printing statistic and WorldDef

--- src/b2_test.c
+++ src/b2_test.c
@@ -1,6 +1,10 @@
 // vim: set colorcolumn=85
 // vim: fdm=marker
 
+// {{{ include
+#include "body.h"
+#include "box2d/id.h"
+#include "box2d/types.h"
 #include "koh.h"
 #include "koh_b2.h"
 #include "koh_common.h"
@@ -23,8 +27,123 @@
 #include <string.h>
 #include "lua.h"
 #include "koh_lua_tools.h"
+// }}}
 
 static bool verbose = false;
+
+static MunitResult test_b2Statistics_to_str(
+    const MunitParameter params[], void* data
+) {
+    b2WorldDef wd = b2DefaultWorldDef();
+    b2WorldId world = b2CreateWorld(&wd);
+    char **lines = b2Statistics_to_str(world, false);
+    munit_assert_ptr_not_null(lines);
+
+    FILE *out;
+    if (verbose)
+        out = stdout;
+    else
+        out = fopen("/dev/null", "w");
+
+    while (*lines) {
+        fprintf(out, "%s\n", *lines);
+        lines++;
+    }
+
+    lines = b2Statistics_to_str(world, true);
+    char *line = calloc(2048, sizeof(char));
+    if (!line)
+        return MUNIT_ERROR;
+
+    // Проверка за выгрузку данных в Луа таблицу
+    strcat(line, "return ");
+    while (*lines) {
+        strcat(line, *lines);
+        lines++;
+    }
+    if (verbose)
+        printf("test_b2Statistics_to_str: line '%s'\n", line);
+    lua_State *l = luaL_newstate();
+    assert(l);
+    luaL_openlibs(l);
+    if (luaL_loadstring(l, line) != LUA_OK) {
+        printf("test_b2Statistics_to_str: %s\n", lua_tostring(l, -1));
+        free(line);
+        lua_close(l);
+        return MUNIT_FAIL;
+    }
+    if (verbose)
+        printf("test_b2Statistics_to_str: [%s]", stack_dump(l));
+    munit_assert(lua_gettop(l) == 1);
+    lua_call(l, 0, 0);
+    munit_assert(lua_gettop(l) == 0);
+    if (verbose)
+        printf("test_b2Statistics_to_str: [%s]", stack_dump(l));
+
+    lua_close(l);
+    free(line);
+
+    fclose(out);
+    b2DestroyWorld(world);
+
+    return MUNIT_OK;
+}
+
+static MunitResult test_b2WorldDef_to_str(
+    const MunitParameter params[], void* data
+) {
+    b2WorldDef wdef = b2DefaultWorldDef();
+    char **lines = b2WorldDef_to_str(wdef, false);
+
+    FILE *out;
+    if (verbose)
+        out = stdout;
+    else
+        out = fopen("/dev/null", "w");
+
+    while (*lines) {
+        fprintf(out, "%s\n", *lines);
+        lines++;
+    }
+
+    if (!verbose)
+        fclose(out);
+
+    lines = b2WorldDef_to_str(wdef, true);
+    char *line = calloc(2048, sizeof(char));
+    if (!line)
+        return MUNIT_ERROR;
+
+    // Проверка за выгрузку данных в Луа таблицу
+    strcat(line, "return ");
+    while (*lines) {
+        strcat(line, *lines);
+        lines++;
+    }
+    if (verbose)
+        printf("test_b2WorldDef_to_str: line '%s'\n", line);
+    lua_State *l = luaL_newstate();
+    assert(l);
+    luaL_openlibs(l);
+    if (luaL_loadstring(l, line) != LUA_OK) {
+        printf("test_b2WorldDef_to_str: %s\n", lua_tostring(l, -1));
+        free(line);
+        lua_close(l);
+        return MUNIT_FAIL;
+    }
+    if (verbose)
+        printf("test_b2WorldDef_to_str: [%s]", stack_dump(l));
+    munit_assert(lua_gettop(l) == 1);
+    lua_call(l, 0, 0);
+    munit_assert(lua_gettop(l) == 0);
+    if (verbose)
+        printf("test_b2WorldDef_to_str: [%s]", stack_dump(l));
+
+    lua_close(l);
+    free(line);
+
+    return MUNIT_OK;
+}
 
 static MunitResult test_b2Vec2_tostr_alloc(
     const MunitParameter params[], void* data
@@ -88,6 +207,22 @@ static MunitTest t_suite_common[] = {
     {
         .name =  (char*) "/test_b2Vec2_tostr_alloc",
         .test = test_b2Vec2_tostr_alloc,
+        .setup = NULL,
+        .tear_down = NULL,
+        .options = MUNIT_TEST_OPTION_NONE,
+        .parameters = NULL,
+    },
+    {
+        .name =  (char*) "/test_b2Statistics_to_str",
+        .test = test_b2Statistics_to_str,
+        .setup = NULL,
+        .tear_down = NULL,
+        .options = MUNIT_TEST_OPTION_NONE,
+        .parameters = NULL,
+    },
+    {
+        .name =  (char*) "/test_b2WorldDef_to_str",
+        .test = test_b2WorldDef_to_str,
         .setup = NULL,
         .tear_down = NULL,
         .options = MUNIT_TEST_OPTION_NONE,
